0ca1223c1510177ac965c2b8fbeea6337644f9d0
LKH <gorloom6425@naver.com>
2020-10-29 18:30:04 +0900

    모놀리식 서비스 구현 (2), 폴더 정리

---
+++ 생활코딩/생활코딩_Nodejs_Cookie&Auth/Nodejs/cookie.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var cookie = require('cookie')
+http.createServer(function (request, response) {
+    var cookies = {}
+    if (request.headers.cookie !== undefined) {
+        cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies.yummy_cookie)
+    response.writeHead(200, {
+        'Set-Cookie': [
+            'yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-age=${60*60*24*30}`,
+            'Secure=Secure; Secure',
+            'HttpOnly=HttpOnly; HttpOnly',
+            'Path=path; Path=/cookie',
+            'Domain=Domain; Domain=o2.org'
+        ]
+    });
+    response.end('Cookie!!');
+}).listen(3000);
